87cc8e887e0d3ce5adc72307fdc79c8abfa25bf1
Walnut_Bagel <saveliev_i_s@mail.ru>
2022-05-21 00:20:41 +0300

    Update main.cs
    ewwgfWS

--- main.cs
+++ main.cs
@@ -1,1 +1,7 @@
 Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello sdgesd!");
+Console.WriteLine("Hello dfg!");
+Console.WriteLine("Hello dfghws!");
